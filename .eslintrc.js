a94b3188a652b4aad6a28307b2f47a550402e1c8
Ramis <rino95@mail.ru>
2023-11-02 09:11:50 +0300

    startNewProject

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {
-    'linebreak-style': 0,
-    'strict': 0,
-    "quotes": ["error", "single"],
-    "no-alert": 0,
-    "no-console": 0,
-    "one-var": 0,
-    "no-plusplus": 0,
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: 'airbnb-base',
+    overrides: [
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'linebreak-style': 0,
+        'strict': 0,
+        "quotes": 0,
+        "no-alert": 0,
+        "no-console": 0,
+        "one-var": 0,
+        "no-plusplus": 0,
+        "indent": ["error", 4],
+        "max-len": 0,
+    },
 };

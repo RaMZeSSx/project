1124690825f43a5d2ad88f6a5fb00123e7262327
Ramis <rino95@mail.ru>
2023-11-04 10:58:26 +0300

    makeTimer

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
     'use.strict';
 
+    // Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
 
     function hideTabContent() {
         tabsContent.forEach((item) => {
-            item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
         tabs.forEach((item) => {
             item.classList.remove('tabheader__item_active');
@@ -15,7 +18,8 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function showTabContent(i = 0) {
-        tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
 
@@ -34,5 +38,61 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
-});
 
+    // Timer
+
+    const deadline = '2023-11-15';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / 1000 / 60 / 60) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            total: t,
+            days: days, // требует сокращенную форму как ниже
+            hours,
+            minutes,
+            seconds,
+        };
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function getZero(num) {
+            if (num < 0) {
+                return (`00`);
+            }
+            if (num < 10) {
+                return (`0${num}`);
+            } else {
+                return num;
+            }
+        }
+
+        function updateClock () {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    
+});
